0770e7cbe0365f586d4b15a3baa8e05462a3f98a
Cosimo Neri <c.neri@itsprodigi.it>
2025-12-12 14:45:32 +0100

    models terminato

--- src/models/Game.java
+++ src/models/Game.java
@@ -7,5 +7,49 @@ public class Game {
 	private int difficulty;
 	
 	
+	public Game(String gameName, String year, int difficulty) {
+		super();
+		this.gameName = gameName;
+		this.year = year;
+		this.difficulty = difficulty;
+	}
+
+
+	public String getGameName() {
+		return gameName;
+	}
+
+
+	public void setGameName(String gameName) {
+		this.gameName = gameName;
+	}
+
+
+	public String getYear() {
+		return year;
+	}
+
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+
+	public int getDifficulty() {
+		return difficulty;
+	}
+
+
+	public void setDifficulty(int difficulty) {
+		this.difficulty = difficulty;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Game [gameName=" + gameName + ", year=" + year + ", difficulty=" + difficulty + "]";
+	}
+	
+	
 	
 }
